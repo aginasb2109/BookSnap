da2f1534759a62085c0dc76154baa5057e048329
Aginas <aginasnathan2109@gmail.com>
2025-03-09 15:16:28 +0530

    changes in env file

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ import express from "express";
 import bodyParser  from "body-parser";
 import pg from "pg";
 import axios from "axios";
+import env from "dotenv";
 
 
 const app=express();
 const port=3000;
 
+env.config();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const db=new pg.Client({
-    user:"postgres",
-    host:"Localhost",
-    database:"BookNotes",
-    password:"Aginas@2109",
-    port:5432,
+    user:process.env.PG_USER,
+    host:process.env.PG_HOST,
+    database:process.env.PG_DATABASE,
+    password:process.env.PG_PASSWORD,
+    port:process.env.PG_PORT,
 })
 
 db.connect();
